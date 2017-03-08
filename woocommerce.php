1eb69eb1923dd2bc969c8b81f7471e39ab7b29ac
vivlonglz <vivlonglz@gmail.com>
2017-03-08 21:25:39 +0800

    Update

--- woocommerce.php
+++ woocommerce.php
@@ -1,16 +1,7 @@
 <?php get_header(); ?>
-<div class="kadima_header_breadcrum_title">	
-	<div class="container">
-		<div class="row">
-			<div class="col-md-12">
-                <?php if (function_exists('kadima_breadcrumbs')) kadima_breadcrumbs(); ?>
-			</div>
-		</div>
-	</div>	
-</div>
 <div class="container">
 	<div class="row kadima_blog_wrapper">
-		<div class="col-md-12">
+		<div class="col-md-12" style="margin-top:100px;">
 			<?php 
 			if( is_shop() ){
 				echo do_shortcode('[ajax_load_more id="productlist" container_type="div" css_classes="col-md-12" post_type="product" posts_per_page="6" images_loaded="true"]');
