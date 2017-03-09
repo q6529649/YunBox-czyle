6b4ab8724b9211ef7ea7a56ea1d260c5c1e7bcf2
vivlonglz <vivlonglz@gmail.com>
2017-03-09 11:48:05 +0800

    Update

--- page.php
+++ page.php
@@ -2,7 +2,7 @@
 get_template_part('breadcrums'); ?>
 <div class="container">
 	<div class="row kadima_blog_wrapper">
-	<div class="col-md-8">
+	<div class="col-md-12">
 	<?php get_template_part('post','page'); ?>	
 	</div>
 	<?php get_sidebar(); ?>	
--- woocommerce.php
+++ woocommerce.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 <div class="container">
-	<div class="row kadima_blog_wrapper">
-		<div class="col-md-12" style="margin-top:100px;">
+	<div class="row">
+		<div class="col-md-12" style="margin-top:50px;">
 			<?php 
 			if( is_shop() ){
 				echo do_shortcode('[ajax_load_more id="productlist" container_type="div" css_classes="col-md-12" post_type="product" posts_per_page="6" images_loaded="true"]');
