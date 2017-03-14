d9e28752945c88d24b08df81cad1b96d79d38298
vivlonglz <vivlonglz@gmail.com>
2017-03-14 10:11:09 +0800

    Update

--- woocommerce.php
+++ woocommerce.php
@@ -1,15 +1,13 @@
 <?php get_header(); ?>
-<div class="container">
-	<div class="row">
-		<div class="col-md-12" style="margin-top:50px;">
-			<?php 
-			if( is_shop() ){
-				echo do_shortcode('[ajax_load_more id="productlist" container_type="div" css_classes="col-md-12" post_type="product" posts_per_page="6" images_loaded="true"]');
-			} else {
-				woocommerce_content();
-			}
-			?>
-		</div>	
-	</div>
-</div>	
+<div class="row">
+	<div class="col-md-12" style="margin-top:50px;">
+		<?php 
+		if( is_shop() ){
+			echo do_shortcode('[ajax_load_more id="productlist" container_type="div" css_classes="col-md-12" post_type="product" posts_per_page="6" images_loaded="true" button_label="Show More"]');
+		} else {
+			woocommerce_content();
+		}
+		?>
+	</div>	
+</div>
 <?php get_footer(); ?>
