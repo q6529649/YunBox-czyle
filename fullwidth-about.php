5285593a4ea8279e16d67ee14d8a8fd1c3d9535b
vivlonglz <vivlonglz@gmail.com>
2017-03-28 17:20:03 +0800

    Update

--- fullwidth-about.php
+++ fullwidth-about.php
@@ -26,8 +26,8 @@ a:hover{color:#F67919; text-decoration:none;}
 ul, li { list-style:none; }
 .Coo_aot{ width:100%; background-color:#fff;padding-top: 100px; }
 .matt{ position:relative; margin:0 auto; height:660px; width:1200px;}
-.eight_lis{  left:0px; top:250px; width:100%;  background:url(./wp-content/themes/YunBox-czyle/images/yuyy.png) no-repeat   center 0px;margin-top: 80px;}
-.feef{ width:57px; height:49px; position:absolute; left:0px; top:595px; background:url(./wp-content/themes/YunBox-czyle/images/flyj.png) no-repeat 0 0; }
+.eight_lis{  left:0px; top:250px; width:100%;  background:url(./wp-content/themes/kadima/images/yuyy.png) no-repeat   center 0px;margin-top: 80px;}
+.feef{ width:57px; height:49px; position:absolute; left:0px; top:595px; background:url(./wp-content/themes/kadima/images/flyj.png) no-repeat 0 0; }
 .uaii{ width:1400px; height:700px; overflow:hidden; margin:0 auto; position:relative;}
 .Coo_aot .taitp{font-size:36px; font-weight:bold; color:#ff7f02; text-align:center; padding-top: 50px;padding-bottom: 20px;  left:0px; opacity:1; z-index:9999; position:relative;}
 .Coo_aot .taitp2{font-size:18px; color:#666; text-align:center; padding:0px 0px; left:0px; opacity:1; z-index:9999; position:relative;}
@@ -84,70 +84,70 @@ ul, li { list-style:none; }
                 </ul>
             </div>
             <div class="yet sing1">
-                <div class="wec te1"><img src="./wp-content/themes/YunBox-czyle/images/dins1.png"/></div>
+                <div class="wec te1"><img src="./wp-content/themes/kadima/images/dins1.png"/></div>
                 <div class="i_t iur1">
                     <h3>2002</h3>
                     <p>YLE built the first factory</p>
                 </div>
             </div>
             <div class="yet sing2">
-                <div class="wec te2"><img src="./wp-content/themes/YunBox-czyle/images/dins2.png"/></div>
+                <div class="wec te2"><img src="./wp-content/themes/kadima/images/dins2.png"/></div>
                 <div class="i_t iur2">
                     <h3>2003</h3>
                     <p>YLE started exportation Business</p>
                 </div>
             </div>
             <div class="yet sing3">
-                <div class="wec te3"><img src="./wp-content/themes/YunBox-czyle/images/dins3.png"/></div>
+                <div class="wec te3"><img src="./wp-content/themes/kadima/images/dins3.png"/></div>
                 <div class="i_t iur3">
                     <h3>2004</h3>
                     <p>YLE built second factory.extend its capacity</p>
                 </div>
             </div>
             <div class="yet sing4">
-                <div class="wec te4"><img src="./wp-content/themes/YunBox-czyle/images/dins4.png"/></div>
+                <div class="wec te4"><img src="./wp-content/themes/kadima/images/dins4.png"/></div>
                 <div class="i_t iur4">
                     <h3>2008</h3>
                     <p>YLE attended CANTON FAIR</p>
                 </div>
             </div>
             <div class="yet sing5">
-                <div class="wec te5"><img src="./wp-content/themes/YunBox-czyle/images/dins4.png"/></div>
+                <div class="wec te5"><img src="./wp-content/themes/kadima/images/dins4.png"/></div>
                 <div class="i_t iur5">
                     <h3>2010</h3>
                     <p>YLE enlarge scale,update equipment</p>
                 </div>
             </div>
             <div class="yet sing6">
-                <div class="wec te6"><img src="./wp-content/themes/YunBox-czyle/images/dins6.png"/></div>
+                <div class="wec te6"><img src="./wp-content/themes/kadima/images/dins6.png"/></div>
                 <div class="i_t iur6">
                     <h3>2011</h3>
                     <p>YLE built a box factory</p>
                 </div>
             </div>
             <div class="yet sing7">
-                <div class="wec te7"><img src="./wp-content/themes/YunBox-czyle/images/dins7.png"/></div>
+                <div class="wec te7"><img src="./wp-content/themes/kadima/images/dins7.png"/></div>
                 <div class="i_t iur7">
                     <h3>2013</h3>
                     <p>YLE bullt a mould factory</p>
                 </div>
             </div>
             <div class="yet sing8">
-                <div class="wec te8"><img src="./wp-content/themes/YunBox-czyle/images/dins8.png"/></div>
+                <div class="wec te8"><img src="./wp-content/themes/kadima/images/dins8.png"/></div>
                 <div class="i_t iur8">
                     <h3>2015</h3>
                     <p>YLE is bullding a 6 floor new factory</p>
                 </div>
             </div>
             <div class="yet sing9">
-                <div class="wec te9"><img src="./wp-content/themes/YunBox-czyle/images/dins9.png"/></div>
+                <div class="wec te9"><img src="./wp-content/themes/kadima/images/dins9.png"/></div>
                 <div class="i_t iur9">
                     <h3>2016</h3>
                     <p>Attended forelgn exhibition show and receive a good achievement</p>
                 </div>
             </div>
             <div class="yet sing10">
-                <div class="wec te10"><img src="./wp-content/themes/YunBox-czyle/images/dins10.png"/></div>
+                <div class="wec te10"><img src="./wp-content/themes/kadima/images/dins10.png"/></div>
                 <div class="i_t iur10">
                     <h3>2017</h3>
                     <p>Import modern equipment from Germany and start working</p>
@@ -166,11 +166,11 @@ ul, li { list-style:none; }
 		</div>
 		<div class="col-md-6" >
 			<div class="col-md-12">
-				<img src="./wp-content/themes/YunBox-czyle/images/cf.jpg"/>
+				<img src="./wp-content/themes/kadima/images/cf.jpg"/>
 			</div>
 		</div>
 	</div>
-	<div style="position: absolute;bottom:-51px;;z-index: 2;background-color:#fff;left: 47%;padding: 20px;border-radius:50%"><img src="./wp-content/themes/YunBox-czyle/images/market.png"></div>
+	<div style="position: absolute;bottom:-51px;;z-index: 2;background-color:#fff;left: 47%;padding: 20px;border-radius:50%"><img src="./wp-content/themes/kadima/images/market.png"></div>
 </div>
 
 <div class="col-md-12" style="background-color:#f5f5f5;padding:80px 0;position:relative;">
@@ -178,31 +178,31 @@ ul, li { list-style:none; }
 		<h1 style="font-size:36px;text-align:center;font-weight: bold;color: #ff7f02;">OUR MARKET</h1>
 		<div class="col-md-12" style="padding-top:50px;padding-bottom: 10px;">
 			<div class="col-md-6">
-				<p><img src="./wp-content/themes/YunBox-czyle/images/tb1.png"/><span style="vertical-align: middle;padding-left:30px;">Width CE,YLE is standard in Europe</span></p>
-				<p><img src="./wp-content/themes/YunBox-czyle/images/tb3.png"/><span style="vertical-align: middle;padding-left:30px;">Asia market is extending by high quality standard</span></p>
+				<p><img src="./wp-content/themes/kadima/images/tb1.png"/><span style="vertical-align: middle;padding-left:30px;">Width CE,YLE is standard in Europe</span></p>
+				<p><img src="./wp-content/themes/kadima/images/tb3.png"/><span style="vertical-align: middle;padding-left:30px;">Asia market is extending by high quality standard</span></p>
 			</div>
 			<div class="col-md-6">
-				<p><img src="./wp-content/themes/YunBox-czyle/images/tb2.png"/><span style="vertical-align: middle;padding-left:30px;">Width CSA-US,YLE sales over USA and Canada</span></p>
-				<p><img src="./wp-content/themes/YunBox-czyle/images/tb4.png"/><span style="vertical-align: middle;padding-left:30px;">Other area include Australia and South American</span></p>
+				<p><img src="./wp-content/themes/kadima/images/tb2.png"/><span style="vertical-align: middle;padding-left:30px;">Width CSA-US,YLE sales over USA and Canada</span></p>
+				<p><img src="./wp-content/themes/kadima/images/tb4.png"/><span style="vertical-align: middle;padding-left:30px;">Other area include Australia and South American</span></p>
 			</div>
 		</div>
-		<div class="col-md-12"><img src="./wp-content/themes/YunBox-czyle/images/xxt.jpg"></div>
+		<div class="col-md-12"><img src="./wp-content/themes/kadima/images/xxt.jpg"></div>
 	</div>	
-	<div style="position: absolute;bottom:-51px;;z-index: 2;background-color:#f5f5f5;left: 47%;padding: 20px;border-radius:50%"><img src="./wp-content/themes/YunBox-czyle/images/team.png"></div>
+	<div style="position: absolute;bottom:-51px;;z-index: 2;background-color:#f5f5f5;left: 47%;padding: 20px;border-radius:50%"><img src="./wp-content/themes/kadima/images/team.png"></div>
 </div>
 
 <div class="col-md-12" style="padding:80px 0;">
 	<div class="container">
 		<h1 style="font-size:36px;text-align:center;font-weight: bold;color: #ff7f02;">TEAM</h1>
 			<div class="col-md-12" style="padding-top:40px;text-align:center;">
-				<div class="col-md-4"><img src="./wp-content/themes/YunBox-czyle/images/zh1.jpg"><p style="padding:20px 0;margin-bottom:0;">YLE is also one of a green sign in CANTON FAIR</p></div>
-				<div class="col-md-4"><img src="./wp-content/themes/YunBox-czyle/images/zh2.jpg"><p style="padding:20px 0;margin-bottom:0;">YLE extend to foreign exhibition shows</p></div>
-				<div class="col-md-4"><img src="./wp-content/themes/YunBox-czyle/images/zh3.jpg"><p style="padding:20px 0;margin-bottom:0;">YLE has its great company culture.Not only working,but also traveling and relaxing</p></div>
+				<div class="col-md-4"><img src="./wp-content/themes/kadima/images/zh1.jpg"><p style="padding:20px 0;margin-bottom:0;">YLE is also one of a green sign in CANTON FAIR</p></div>
+				<div class="col-md-4"><img src="./wp-content/themes/kadima/images/zh2.jpg"><p style="padding:20px 0;margin-bottom:0;">YLE extend to foreign exhibition shows</p></div>
+				<div class="col-md-4"><img src="./wp-content/themes/kadima/images/zh3.jpg"><p style="padding:20px 0;margin-bottom:0;">YLE has its great company culture.Not only working,but also traveling and relaxing</p></div>
 			</div>
 			<div class="col-md-12" style="padding-top:40px;text-align:center;">
-				<div class="col-md-4"><img src="./wp-content/themes/YunBox-czyle/images/zh1.jpg"><p style="padding:20px 0;margin-bottom:0;">Learning and Training</p></div>
-				<div class="col-md-4"><img src="./wp-content/themes/YunBox-czyle/images/zh2.jpg"><p style="padding:20px 0;margin-bottom:0;">Contest and competition</p></div>
-				<div class="col-md-4"><img src="./wp-content/themes/YunBox-czyle/images/zh3.jpg"><p style="padding:20px 0;margin-bottom:0;">Party</p></div>
+				<div class="col-md-4"><img src="./wp-content/themes/kadima/images/zh1.jpg"><p style="padding:20px 0;margin-bottom:0;">Learning and Training</p></div>
+				<div class="col-md-4"><img src="./wp-content/themes/kadima/images/zh2.jpg"><p style="padding:20px 0;margin-bottom:0;">Contest and competition</p></div>
+				<div class="col-md-4"><img src="./wp-content/themes/kadima/images/zh3.jpg"><p style="padding:20px 0;margin-bottom:0;">Party</p></div>
 			</div>
 	</div>	
 </div>
